379fe743b0938b53f3ede73391d356af191e95da
oli-viaaaa <4688ehowl@naver.com>
2023-04-26 18:07:16 +0900

    chap10_servlet02 update

---
+++ src/main/java/com/javalab/servlet/LoginServlet.java
@@ -0,0 +1,73 @@
+package com.javalab.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+// @WebServlet("/login") 이런 요청이 들어오면 내가 받겠다.
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 파라미터 인코딩
+		request.setCharacterEncoding("utf-8");
+		
+		// 웹브라우저에 응답 인코딩
+		response.setContentType("text/html;charset=utf-8");
+		
+		PrintWriter out = response.getWriter();
+		
+		String id = request.getParameter("user_id");
+		String pw = request.getParameter("user_pw");
+		
+		String strData = "<htmal>";
+		strData += "<body>";
+		strData += "아이디 : " + id;
+		strData += "<br>";
+		strData += "비밀번호 : "+ pw;
+		strData += "</html>";
+		strData += "</body>";
+		out.print(strData);
+	}
+	
+	public void destory() {
+		System.out.println("destory 메서드 호출");
+	}
+
+}
